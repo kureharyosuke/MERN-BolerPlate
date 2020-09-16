ad62f149cd0fb42f81bfb0789fb6093897c4c626
kureharyosuke <ohhayo.com@gmail.com>
2020-09-16 18:50:31 +0900

    #7. Bcrypt is Password Setting

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +14,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 8,
+    minlength: 10,
   },
   lastname: {
     type: String,
@@ -30,6 +32,25 @@ const userSchema = mongoose.Schema({
     //유효기간
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  let user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호 암호화 설정
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
